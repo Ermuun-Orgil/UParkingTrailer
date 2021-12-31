60f5365f89e0aa867f17f9abd4049fff222337de
IceCream <icecream@IceCreams-MacBook-Pro.local>
2021-12-31 11:39:35 +0800

    ok

---
+++ build/precache-manifest.4fc1bf8d6dc55f25730201cd3e42e62c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecbc9155265f2d99de699d8b3d047297",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a741b3af2f609918cf97",
+    "url": "/static/css/main.06a7e52a.chunk.css"
+  },
+  {
+    "revision": "2ecc42abe95ad5855e04",
+    "url": "/static/js/2.5f354449.chunk.js"
+  },
+  {
+    "revision": "a741b3af2f609918cf97",
+    "url": "/static/js/main.e68441ee.chunk.js"
+  },
+  {
+    "revision": "9e617fbff5cce866823f",
+    "url": "/static/js/runtime~main.5d230c4a.js"
+  }
+]);
